dd60015bd8e86e46743c09dad0b227e2b1dbd5cc
Stevo5o <stevo.joc@gmail.com>
2015-03-12 21:38:05 +0000

    Media

--- public_html/script.js
+++ public_html/script.js
@@ -10,7 +10,28 @@
 // immediately invoked anonymous function
 ( function () {
 
-
+// jQuery AJAX call
+$.ajax({
+	url: 'rtj2.json',
+	dataType: 'json',
+	type: 'get',
+	cache: false,
+	success: function(data) {
+		console.log(data);
+		// display links on page
+		var output = "";
+		$.each(data, function(key, val) {
+			for (var i = 0; i < val.length; i++) {
+				var user = val[i];
+				output +=
+					"<li><a id='" +
+					user.rtj2 +					
+					"</a></li>";
+			}
+			$('ul#rtj2').append(output);
+		});
+	}
+});
 
 }() ); // end immediately invoked anonymous function
 
